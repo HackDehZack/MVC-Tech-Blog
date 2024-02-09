149815a5d0969aea20946cdb9d5e27bfc1346492
HackDehZack <Zackseriousemail@gmail.com>
2024-02-08 20:07:46 -0500

    made css and js folders in public folder, did basic js work for public f

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+const submit = document.getElementById('add-comment');
+
+const addComment = async ()=> {
+const comment = document.getElementById('blog-comment');
+const addCommentBtn = document.getElementById('add-comment');
+const blogId = addCommentBtn.dataset.blogid;
+
+if (comment) {
+
+    const response = await fetch (`/api/post/${blogId}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            comment: comment.value
+        }), 
+        headers: {'Content-Type': 'application/json'}
+    })
+    if (response.ok){
+        alert("Comment has been successfully added to this post!");
+        location.reload();
+    } else {
+        alert('Error occurred, failed to add comment')
+    }
+}
+}
+
+submit.addEventListener('click', (addComment))
