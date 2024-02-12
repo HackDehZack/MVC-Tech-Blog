3a7753179ced1684a7e4714d9b56eed72a20c917
HackDehZack <Zackseriousemail@gmail.com>
2024-02-12 14:01:51 -0500

    Update server.js to use PORT environment variable

--- server.js
+++ server.js
@@ -58,8 +58,9 @@ app.use(routes);
 
 // Sync sequelize models and then start the server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on: http://localhost:${PORT}`);
+    const port = process.env.PORT || 3000; // In case the PORT env variable is not set, fallback to 3000
+    app.listen(port, () => {
+        console.log(`Server running on: http://localhost:${port}`);
         console.log('Create an account to start your blog!');
     });
 });
